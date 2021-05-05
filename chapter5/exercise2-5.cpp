78288b4e46d2336ccd0c91ed04084a14d8298391
Xamarsia <xamarsia@gmail.com>
2021-05-05 22:23:47 +0300

    Added check to main() function.

--- chapter5/exercise2-5.cpp
+++ chapter5/exercise2-5.cpp
@@ -3,6 +3,11 @@
 // текст содержит много ошибок. Найдите ошибки. перечислите их и ис­
 // правьте программу.
 
+// 3. Самой низкой температурой является абсолютный нуль, т.е. -273. 15°C,
+// или ОК. Даже после исправления приведенная выше программа выво­
+// дит неверные результаты для температуры ниже абсолютного нуля.
+// Поместите в функцию main ( ) проверку, которая будет выводить сооб­
+// щение об ошибке для температур ниже -273. 1 5°С.
 
 #include <iostream>
 
@@ -18,7 +23,12 @@ int main()
     double celsius;
     std::cout << "Enter the temperature in Celsius." << std::endl;
     std::cin >> celsius;
-    double kelvin;
+    double kelvin = toKelvin(celsius); 
+    if (kelvin < -273.15) //  task 5
+    {
+        std::cout << "Error, there is no such low temperature." << std::endl;
+        return 0;
+    }
     std::cout << "Your temperature in degrees Kelvin is " << kelvin << '.' << std::endl;
 
     return 0;
