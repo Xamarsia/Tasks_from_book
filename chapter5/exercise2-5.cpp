6a7fb12dd6221ab72388adbcc621888190fe5f8b
Xamarsia <xamarsia@gmail.com>
2021-05-05 23:17:58 +0300

    Еhe program converts the temperature in Kelvin to the temperature in Celsius.

--- chapter5/exercise2-5.cpp
+++ chapter5/exercise2-5.cpp
@@ -9,40 +9,42 @@
 // Поместите в функцию main ( ) проверку, которая будет выводить сооб­
 // щение об ошибке для температур ниже -273. 1 5°С.
 
-
 // 4. Повторите упр. 3 , но в этот раз ошибку обрабатывайте в функции
 // ctok().
 
+// 5. Измените программу так. чтобы она преобразовывала температуру в
+// градусах Кельвина в температуру в градусах Цельсия.
 
 #include <iostream>
 
-double toKelvin(double celsiusT)
+double toCelsius(double kelvinT)
 {
-    double kelvinT = celsiusT + 273.15;
-    if (kelvinT < -273.15) //  task 4
+    double celsiusT = kelvinT - 273.15;
+    if (kelvinT < -273.15)
     {
         throw std::invalid_argument("Incorrectly entered temperature!Such a low temperature does not exist.");
     }
-    return kelvinT;
+    return celsiusT;
 }
 
 
 int main()
 {
-    double celsius;
-    std::cout << "Enter the temperature in Celsius." << std::endl;
-    std::cin >> celsius;
     double kelvin;
+    std::cout << "Enter the temperature in Kelvin." << std::endl;
+    std::cin >> kelvin;
+    double celsius;
     try
     { 
-        kelvin = toKelvin(celsius);
+        kelvin = toCelsius(kelvin);
     }
 
     catch (std::invalid_argument &exception)
     {
         std::cerr << exception.what() << std::endl;
     }
-    std::cout << "Your temperature in degrees Kelvin is " << kelvin << '.' << std::endl;
+    
+    std::cout << "Your temperature in degrees Celsius is " << kelvin << '.' << std::endl;
 
     return 0;
 }
